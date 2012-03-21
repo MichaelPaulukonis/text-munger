f5e85282e2a589102a18be1f7830647a55405c18
xraysmalevich@gmail.com <xraysmalevich@gmail.com@fec1b8e5-8731-27d8-2958-0cb2ef2a6579>
2012-03-21 17:17:09 +0000

    Started extracting Fetch components from Runner into new TextSourcers project. Initially called "TextSourcerers" so you still see that as the folder name. Hrm.
    Library may be extracted from TextTRansformers into TextSources.
    
    Beginnings of XrmlFetch and GutenbergFetch -- need an interface to group them under
    
    Back in Trandformers, Beginning of a descriptive-but-poorly-named class "TransformerFromFile" based on the Homophonic transformer.
    
    Also renamed Runner.TextMunger.cs := ConsoleRunner.cs
    
    Despite the impending movement of all pieces out of it, keep it around for console testing, and projected future scriptability tests.

---
+++ PseudoRandomTextGenerator/trunk/PseudoRandomTextGenerator/TransformerFromFile.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TextTransformer
+{
+    // TODO: instead of just homophones, this would lend itself to ANY word-level, list-based transformation
+    //       ie, mis-spellings, etc.
+    //       NOT, however, leet-speak or other REGEX-level transformers
+    //       or.... would it???
+    public class TransformerFromFile : ITransformer
+    {
+        public TransformerFromFile(string sourceFile)
+        {
+            // eg  @"D:\Dropbox\projects\TextMunger\homophone_list.txt";
+            SourceFile = sourceFile;
+
+            // TODO: Smart/Dumb setting: smart = whole words only, dumb = inner-strings
+            //       so we get classic := clbuttic
+        }
+
+        public string SourceFile { get; private set; }
+
+        public string Source { get; set; }
+
+        private string _m = null;
+
+        public string Munged
+        {
+            get { return Munge(); }
+        }
+
+        private string Munge()
+        {
+            // if we process multiple words, add a space as padding
+            // if we process a single word, no padding
+            // and, yeah, this will only work on space-padded multi-words. c'est la vie.
+            var words = TransformerTools.SplitToWords(Source);
+            var padding = (words.Any()) ? " " : string.Empty;
+
+            var sb = new StringBuilder();
+            var rnd = new Random();
+
+            foreach (var word in words)
+            {
+                var replace = word;
+                // if word is in dictionary
+                // replace with a homophone
+                // if multiples, random of quantity
+                if (Homophones.ContainsKey(word))
+                {
+                    var index = rnd.Next(0, Homophones[word].Count); // random.next range := 0..(Count-1)
+                    replace = Homophones[word][index];
+                }
+                sb.Append(replace + padding);
+            }
+
+            return sb.ToString();
+        }
+
+        public Granularity Granularity { get { return Granularity.Word; } }
+
+        private Dictionary<string, List<string>> _homophones = null;
+
+        private Dictionary<string, List<string>> Homophones
+        {
+            get { return _homophones ?? (_homophones = GetHomophones()); }
+        }
+
+        // TODO: replace "homophones" with something meaningful
+        // pairs? ugh. word-tuples? word-pairs?
+        private Dictionary<string, List<string>> GetHomophones()
+        {
+            var homophones = new Dictionary<string, List<string>>();
+
+            using (var reader = new StreamReader(SourceFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#")) { continue; } // comment-character
+
+                    var pieces = line.Split(',');
+
+                    var variants = Permutations(pieces);
+                    foreach (var variant in variants)
+                    {
+                        var key = variant.ElementAt(0).Trim();
+                        if (!homophones.ContainsKey(key))
+                        {
+                            var vals = variant.Skip(1).Take(variant.Count() - 1).ToList();
+                            homophones.Add(key, vals);
+                        }
+                    }
+                }
+            }
+
+            return homophones;
+        }
+
+        // http://stackoverflow.com/a/5129643/41153
+        private static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> source)
+        {
+            var c = source.Count();
+            if (c == 1)
+                yield return source;
+            else
+                for (int i = 0; i < c; i++)
+                    foreach (var p in Permutations(source.Take(i).Concat(source.Skip(i + 1))))
+                        yield return source.Skip(i).Take(1).Concat(p);
+        }
+
+        public override string ToString()
+        {
+            return "Homphonerize";
+        }
+    }
+}
---
+++ PseudoRandomTextGenerator/trunk/TextSourcerers/XrmlFetch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace TextSourcers
+{
+    public class XrmlFetch
+    {
+        // sample use: _textCache = mg.GetRandomContents(GetXrmlPages(), 60);
+        // unfortunately, that relied on randomization... instead of a mechanism for ALL
+
+        // TODO: build interfaces for getting all contents, random contents, and others
+        // return as Text sources
+        // which will have to be updated to use an optional URL instead of just file
+
+        private string GetRandomContents(IEnumerable<string> urls, int number)
+        {
+            // http://stackoverflow.com/questions/48087/select-a-random-n-elements-from-listt-in-c-sharp
+            var rnd = new Random();
+            var subUrls = urls.OrderBy(x => rnd.Next()).Take(5).ToArray<string>();
+
+            return GetXrmlWebText(subUrls);
+        }
+
+        private static string GetXrmlWebText(IEnumerable<string> urls)
+        {
+            const string xpath = @"//div[@style='font-family: monospace;']";
+            var sb = new StringBuilder();
+            foreach (var url in urls)
+            {
+                try
+                {
+                    var source = WebFetch.Fetch(url);
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(source);
+                    var target = doc.DocumentNode.SelectSingleNode(xpath);
+                    var text = HttpUtility.HtmlDecode(target.InnerText);
+                    sb.Append(text);
+                }
+                catch (Exception ex)
+                {
+                    // TODO: swallow it whole!
+                    // yikes. or maybe log the issue, down the road....
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        // perhaps cache this list locally, and only regen as required?
+        // because it's a pain to pull....
+        // that should be true of all of of web-pulls
+        // cache locally, and only rebuild as required.
+        // hrm.... MORE COMPLICATED THAT I CURRENTLY REQUIRE
+        private static List<string> GetXrmlPages()
+        {
+            var source = WebFetch.Fetch("http://www.xradiograph.com/XraysMonaLisa/MappaMundi");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(source);
+            var target = doc.DocumentNode.SelectNodes("//div[@class='mainlist']/div/ul/li/a");
+
+            return target.Select(link => link.Attributes["href"].Value).ToList();
+        }
+    }
+}
