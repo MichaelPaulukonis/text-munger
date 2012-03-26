4431f8a29f6e81e7524f3a0ea3ebeae30eb89be8
xraysmalevich@gmail.com <xraysmalevich@gmail.com@fec1b8e5-8731-27d8-2958-0cb2ef2a6579>
2012-03-26 18:37:55 +0000

    working on adding serialization to Texts and libraries Can serialize a text (no save/load tested -- only to JSON string and back) But can't serialize the enclosing library, which is half the point (ie, a library full of texts). Dictionary<string, Text> seems to be the issue. Although Text is serializable, the dictionary is NOT, using this method?
    Look into using JavaScriptSerializer or going to XMLSerializer
    
    Test code added in ConsoleRunner.
    Extension methods added to TextSourcers
    Would have added to GUI, but that meant that TextSources needed to reference GUI, and that was a dis-allowed circular reference.
    Might have added some references that are no longer needed as locations changed.....

---
+++ PseudoRandomTextGenerator/trunk/TextSourcerers/ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace TextSourcers
+{
+    public static class ExtensionMethods
+    {
+        // via http://www.joe-stevens.com/2009/12/29/json-serialization-using-the-datacontractjsonserializer-and-c/
+        public static string ToJSON<T>(this T obj) where T : class
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, obj);
+                return Encoding.Default.GetString(stream.ToArray());
+            }
+        }
+
+        public static T FromJSON<T>(this T obj, string json) where T : class
+        {
+            using (var stream = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return serializer.ReadObject(stream) as T;
+            }
+        }
+    }
+}
