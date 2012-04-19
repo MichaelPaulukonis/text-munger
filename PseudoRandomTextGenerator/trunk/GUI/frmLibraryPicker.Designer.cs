ead4213b314f474b406eb161934a95a377ebae56
xraysmalevich@gmail.com <xraysmalevich@gmail.com@fec1b8e5-8731-27d8-2958-0cb2ef2a6579>
2012-04-19 17:13:46 +0000

    updated CustomSelectControl. Rebuilt frmLibraryPicker to use docking -- had to add some panels. Resizing form now allows control to auto-resize, as well as draggable-split between the two windows. The file-names are now fully visible!
    TODO:The button layout needs some work.
    TODO: no other edito-form has been updated with docking

--- PseudoRandomTextGenerator/trunk/GUI/frmLibraryPicker.Designer.cs
+++ PseudoRandomTextGenerator/trunk/GUI/frmLibraryPicker.Designer.cs
@@ -35,20 +35,26 @@
             this.saveLibraryDialog = new System.Windows.Forms.SaveFileDialog();
             this.openLibraryDialog = new System.Windows.Forms.OpenFileDialog();
             this.btnLoad = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panelButtons.SuspendLayout();
             this.SuspendLayout();
             // 
             // LibrarySelector
             // 
             this.LibrarySelector.AllowMultipleItems = true;
-            this.LibrarySelector.Location = new System.Drawing.Point(0, -1);
+            this.LibrarySelector.DisplayFilter = true;
+            this.LibrarySelector.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LibrarySelector.Location = new System.Drawing.Point(3, 3);
             this.LibrarySelector.Name = "LibrarySelector";
             this.LibrarySelector.SelectedItems = ((System.Collections.Generic.List<object>)(resources.GetObject("LibrarySelector.SelectedItems")));
-            this.LibrarySelector.Size = new System.Drawing.Size(389, 255);
+            this.LibrarySelector.Size = new System.Drawing.Size(464, 232);
             this.LibrarySelector.TabIndex = 0;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(314, 260);
+            this.btnExit.Location = new System.Drawing.Point(388, 3);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 1;
@@ -58,7 +64,7 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(152, 260);
+            this.btnSave.Location = new System.Drawing.Point(87, 3);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 2;
@@ -77,7 +83,7 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(71, 260);
+            this.btnLoad.Location = new System.Drawing.Point(3, 3);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(75, 23);
             this.btnLoad.TabIndex = 3;
@@ -85,17 +91,50 @@
             this.btnLoad.UseVisualStyleBackColor = true;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panelButtons, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LibrarySelector, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(470, 268);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.ColumnCount = 3;
+            this.panelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.81818F));
+            this.panelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78.18182F));
+            this.panelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.panelButtons.Controls.Add(this.btnLoad, 0, 0);
+            this.panelButtons.Controls.Add(this.btnExit, 2, 0);
+            this.panelButtons.Controls.Add(this.btnSave, 1, 0);
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtons.Location = new System.Drawing.Point(0, 238);
+            this.panelButtons.Margin = new System.Windows.Forms.Padding(0);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.RowCount = 1;
+            this.panelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.panelButtons.Size = new System.Drawing.Size(470, 30);
+            this.panelButtons.TabIndex = 0;
+            // 
             // frmLibraryPicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 299);
-            this.Controls.Add(this.btnLoad);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.LibrarySelector);
+            this.ClientSize = new System.Drawing.Size(470, 268);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frmLibraryPicker";
             this.Text = "LibrarySelector";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panelButtons.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -108,5 +147,7 @@
         private System.Windows.Forms.SaveFileDialog saveLibraryDialog;
         private System.Windows.Forms.OpenFileDialog openLibraryDialog;
         private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel panelButtons;
     }
 }
