540685ba12728f782ddd9cbb401672ea8c39b87b
xraysmalevich@gmail.com <xraysmalevich@gmail.com@fec1b8e5-8731-27d8-2958-0cb2ef2a6579>
2012-04-24 02:51:46 +0000

    fixed up the rule-set editor. It looks nicer, but not awesome. The LibraryFetcher now ignore the root-Library if it does not contain .txt files.

--- trunk/PseudoRandomTextGenerator/trunk/GUI/RuleSetEditor.Designer.cs
+++ trunk/PseudoRandomTextGenerator/trunk/GUI/RuleSetEditor.Designer.cs
@@ -37,22 +37,30 @@
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.txtSetName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Editor
             // 
             this.Editor.AllowMultipleItems = true;
-            this.Editor.Location = new System.Drawing.Point(-3, 0);
+            this.Editor.DisplayFilter = true;
+            this.Editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Editor.Location = new System.Drawing.Point(3, 30);
             this.Editor.Name = "Editor";
             this.Editor.SelectedItems = ((System.Collections.Generic.List<object>)(resources.GetObject("Editor.SelectedItems")));
-            this.Editor.Size = new System.Drawing.Size(389, 255);
+            this.Editor.Size = new System.Drawing.Size(532, 242);
             this.Editor.TabIndex = 0;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(168, 261);
+            this.btnSave.Location = new System.Drawing.Point(83, 3);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(74, 23);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "&Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -60,7 +68,8 @@
             // 
             // btnUseSet
             // 
-            this.btnUseSet.Location = new System.Drawing.Point(296, 261);
+            this.btnUseSet.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnUseSet.Location = new System.Drawing.Point(460, 3);
             this.btnUseSet.Name = "btnUseSet";
             this.btnUseSet.Size = new System.Drawing.Size(75, 23);
             this.btnUseSet.TabIndex = 2;
@@ -70,9 +79,9 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(87, 261);
+            this.btnLoad.Location = new System.Drawing.Point(3, 3);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.Size = new System.Drawing.Size(62, 23);
             this.btnLoad.TabIndex = 3;
             this.btnLoad.Text = "&Load";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -89,7 +98,7 @@
             // 
             // txtSetName
             // 
-            this.txtSetName.Location = new System.Drawing.Point(226, 22);
+            this.txtSetName.Location = new System.Drawing.Point(139, 3);
             this.txtSetName.Name = "txtSetName";
             this.txtSetName.Size = new System.Drawing.Size(144, 20);
             this.txtSetName.TabIndex = 4;
@@ -98,27 +107,72 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(175, 28);
+            this.label1.Location = new System.Drawing.Point(82, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "SetName";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Editor, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(538, 304);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnUseSet, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnSave, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnLoad, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 275);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(538, 29);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtSetName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(252, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 27);
+            this.panel1.TabIndex = 7;
             // 
             // RuleSetEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 292);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtSetName);
-            this.Controls.Add(this.btnLoad);
-            this.Controls.Add(this.btnUseSet);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.Editor);
+            this.ClientSize = new System.Drawing.Size(538, 304);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "RuleSetEditor";
             this.Text = "RuleSetEditor";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -132,5 +186,8 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
         private System.Windows.Forms.TextBox txtSetName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
